a07e443c9b1140c0c9232610bef2004f7cf478b3
Alfred Kyle <1106592673@qq.com>
2022-07-19 17:06:51 +0800

    2022.7.19 在front中新增加了联系我们

--- vue/src/views/front/Front.vue
+++ vue/src/views/front/Front.vue
@@ -17,7 +17,7 @@
         >
           <el-menu-item index="1" @click="$router.push('/front/home')">主页</el-menu-item>
           <el-menu-item index="2">讨论</el-menu-item>
-          <el-menu-item index="3">联系我们</el-menu-item>
+          <el-menu-item index="3">关于</el-menu-item>
         </el-menu>
         <div class="search">
           <el-input
@@ -51,27 +51,109 @@
             </div>
             </div>
     </div>
+    <el-dialog
+        v-model="dialogVisible"
+        title="联系我们"
+        width="700px"
+        center
+        top="25vh"
+        show-close
+    >
+      <el-form :model="form" :rules="rules" ref="form">
+        <el-form-item label="反馈类型" :label-width="'150px'" prop="type">
+          <el-radio-group v-model="form.type">
+            <el-radio label="界面UI"/>
+            <el-radio label="使用体验"/>
+            <el-radio label="Bug反馈"/>
+            <el-radio label="其他问题"/>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="联系方式" :label-width="'150px'" prop="number">
+          <el-input v-model="form.number" placeholder="请输入您邮箱或者电话" style="width: 80%">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="反馈内容" :label-width="'150px'" prop="content">
+          <el-input type="textarea" v-model="form.content" placeholder="请输入您的反馈内容" style="width: 80%;"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submit"
+        >提交</el-button
+        >
+      </span>
+      </template>
+    </el-dialog>
+    <el-dialog
+      v-model="dialogVisible1"
+      width="700px"
+      center
+      top="25vh"
+    >
+      <el-result
+          icon="success"
+          title="提交成功"
+          sub-title="我们会尽快联系您，请注意查看您的邮箱或来电"
+      >
+        <template #extra>
+          <el-button type="primary" @click="dialogVisible1 = false">返回</el-button>
+        </template>
+      </el-result>
+    </el-dialog>
     <div style="width:1400px;margin: 15px auto;">
+      <div class="bar">
+        <el-tooltip
+            class="box-item"
+            effect="light"
+            content="联系我们"
+            placement="top-end"
+        >
+          <el-button circle type="primary"
+                     style="width: 60px;height: 60px;font-size: 30px"
+                     @click="dialogVisible = true"
+          ><el-icon><Iphone /></el-icon></el-button>
+        </el-tooltip>
+      </div>
       <router-view/>
     </div>
   </div>
 </template>
 
 <script>
-import {ArrowDown, Avatar} from '@element-plus/icons-vue'
-import {RemoveFilled} from "@element-plus/icons";
+import {Iphone, RemoveFilled} from "@element-plus/icons";
 export default {
   name: "Front",
   components: {
+    Iphone,
     RemoveFilled,
-    Avatar,
-    ArrowDown,
   },
   data() {
     return {
+      select:'',
+      feedback:'',
+      type:'',
+      form:{},
+      dialogVisible:false,
+      dialogVisible1:false,
       activeIndex:'0',
       search: '',
-      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {},
+      rules: {  //验证规则
+        type: [
+          {
+            required: true,
+            message: '请选择您要反馈的类型',
+            trigger: 'change',
+          },
+        ],
+        number: [
+          { required: true, message: '请输入您的联系方式', trigger: 'blur' },
+        ],
+        content: [
+          { required: true, message: '请输入您宝贵的意见或建议', trigger: 'blur' },
+        ],
+      },
     }
   },
   created() {
@@ -79,6 +161,15 @@ export default {
     this.user = JSON.parse(userStr)
   },
   methods: {
+    submit() {
+      this.$refs['form'].validate((valid) => {
+        if(valid) {   //判断是否满足验证规则，才能进行下面的请求
+          this.dialogVisible = false;
+          this.dialogVisible1 = true;
+          this.form = {};//清空表单
+          }
+        })
+    },
     logout() {
       this.$router.push("/login")
       localStorage.removeItem("user")
@@ -135,5 +226,11 @@ el-dropdown {
   height: 60px;
   float:left;
   width: 30%;
+}
+.bar {
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  z-index: 2;
 }
 </style>
