8ca66a65fb50a20963b7e4c3c2e341fd9feb359e
JHJ <3132343887@qq.com>
2022-08-03 15:35:02 +0800

    2022.8.03 个人空间

--- springboot/src/main/java/com/example/demo/entity/User.java
+++ springboot/src/main/java/com/example/demo/entity/User.java
@@ -19,6 +19,8 @@ public class User {
     private String address;
     private String avatarUrl;
     private String  role;
-
+    private String  telephone;
+    private String  introduce;
+    private String  birthday;
 
 }
