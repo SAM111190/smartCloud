10c3d88dbe2f317b344fc8096e7bd5d7c47174cf
ZhangWeiEcust <1736482740@qq.com>
2022-08-20 15:24:59 +0800

    8、20 公告栏和讨论区

---
+++ springboot/src/main/java/com/example/demo/controller/BulletinController.java
@@ -0,0 +1,69 @@
+package com.example.demo.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
+import javax.annotation.Resource;
+import java.util.List;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.common.Result;
+
+import com.example.demo.service.IBulletinService;
+import com.example.demo.entity.Bulletin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author ZW
+ * @since 2022-08-19
+ */
+@RestController
+@RequestMapping("//bulletin")
+        public class BulletinController {
+    
+@Resource
+private IBulletinService bulletinService;
+
+// 新增或者更新
+@PostMapping
+public Result save(@RequestBody Bulletin bulletin) {
+    bulletinService.saveOrUpdate(bulletin);
+        return Result.success();
+        }
+
+@DeleteMapping("/{id}")
+public Result delete(@PathVariable Integer id) {
+    bulletinService.removeById(id);
+        return Result.success();
+        }
+
+@PostMapping("/del/batch")
+public Result deleteBatch(@RequestBody List<Integer> ids) {
+    bulletinService.removeByIds(ids);
+        return Result.success();
+        }
+
+@GetMapping
+public Result findAll() {
+        return Result.success(bulletinService.list());
+        }
+
+@GetMapping("/{id}")
+public Result findOne(@PathVariable Integer id) {
+        return Result.success(bulletinService.getById(id));
+        }
+
+@GetMapping("/page")
+public Result findPage(@RequestParam Integer pageNum,
+@RequestParam Integer pageSize) {
+        QueryWrapper<Bulletin> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("id");
+        return Result.success(bulletinService.page(new Page<>(pageNum, pageSize), queryWrapper));
+        }
+
+        }
+
---
+++ springboot/src/main/java/com/example/demo/service/IBulletinService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Bulletin;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author ZW
+ * @since 2022-08-19
+ */
+public interface IBulletinService extends IService<Bulletin> {
+
+}
