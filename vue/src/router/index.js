39a55f15fb36df2fb6b28bda364155c62fe089ac
JHJ <3132343887@qq.com>
2023-02-04 15:19:06 +0800

    2023.2.4 论坛后台管理

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -29,6 +29,11 @@ const routes = [
           name: 'Bulletin',
           component: () => import("@/views/background/Bulletin"),
         },
+        {
+          path: 'forum',
+          name: 'Forum',
+          component: () => import("@/views/background/Forum"),
+        },
       ]
   },
   {
